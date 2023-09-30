04c7ef53487359dbcb0c790e40ddcb7ec61e0f02
Infinite00loop <146521320+Infinite00loop@users.noreply.github.com>
2023-09-30 14:14:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function printDetails(){
+    console.log(document.getElementById('idx1').value);
+    console.log(document.getElementById('idx2').value);
+    console.log(document.getElementById('idx3').value);
+    console.log(document.getElementById('idx4').value);
+    console.log(document.getElementById('idx5').value);
+}
